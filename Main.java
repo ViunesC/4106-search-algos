15ffc1abccbadabf16ba34ac986b8eb49134d7cb
ViunesC <mrfusking233@gmail.com>
2024-12-11 19:37:43 -0500

    update

--- Main.java
+++ Main.java
@@ -1,8 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        Maze mymaze = Maze.createMaze("mymaze2.maze");
+        Maze mymaze = Maze.createMaze("gpt200x200.maze");
         // System.out.println(mymaze);
 
+        mymaze.bfs(0,0, false);
+        System.out.println("");
         mymaze.a_star(0,0, false);
     }
 }
